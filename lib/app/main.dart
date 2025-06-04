be2a279522471f3b6fbfbfb2fecce15514cf908e
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-06-04 18:53:19 +0900

    chore: kakaoSdk initialize

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_template.dart';
 import 'package:todomodu_app/app/todomodu_app.dart';
 import 'package:todomodu_app/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']!);
   runApp(ProviderScope(child: const TodomoduApp()));
 }
