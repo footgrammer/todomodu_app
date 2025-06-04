7966036b9eb7d9471f5772586ff5ec45e673e5ae
kihyun <=>
2025-06-04 17:45:51 +0900

    [app] 엔티티 초안 작성 Fixes #11

---
+++ lib/features/todo/domain/entities/todo.dart
@@ -0,0 +1,19 @@
+import 'package:todomodu_app/features/sub_task/domain/entities/sub_task.dart';
+
+class Todo {
+  String id;
+  String projectId;
+  String title;
+  List<SubTask> subTasks;
+  DateTime startDate;
+  DateTime endDate;
+
+  Todo({
+    required this.id,
+    required this.projectId,
+    required this.title,
+    required this.subTasks,
+    required this.startDate,
+    required this.endDate,
+  });
+}
