7eb7b2f2f173368cc33444fba26f5ba81eaaf45a
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-06-04 18:38:27 +0900

    chore: wrap app with ProviderScope

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todomodu_app/app/todomodu_app.dart';
 
 void main() {
-  runApp(const TodomoduApp());
+  runApp(ProviderScope(child: const TodomoduApp()));
 }
