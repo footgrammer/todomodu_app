9b35078785b40238bfe08872143c12cfa00fc4cf
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-06-04 18:42:00 +0900

    chore: 앱 시작 시 firebase 초기화

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todomodu_app/app/todomodu_app.dart';
+import 'package:todomodu_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ProviderScope(child: const TodomoduApp()));
 }
