f3ec1e59a85b78721b92948e382a6f77a7cf1cc4
undercrosshd <hd907@naver.com>
2025-06-05 16:38:27 +0900

    design : 시작일, 종료일 UI 구현

--- lib/features/todo/presentation/pages/add_todo.dart
+++ lib/features/todo/presentation/pages/add_todo.dart
@@ -16,22 +16,84 @@ class AddTodoPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('할 일 이름',
-            style: TextStyle(fontSize: 16,
-            fontWeight: FontWeight.bold),
+            const Text(
+              '할 일 이름',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-            TextField(decoration: InputDecoration(
-              hintText: '할 일 1',
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide.none),
-              contentPadding: const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
-              filled: true,
-              fillColor: Colors.grey[150],
-            ))
+            TextField(
+              decoration: InputDecoration(
+                hintText: '할 일 1',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none),
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
+                filled: true,
+                fillColor: Colors.grey[300],
+              ),
+            ),
+            const SizedBox(height: 24),
+            Row(
+              children: [
+                Expanded(
+                  child: const Text(
+                    '시작일',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(width: 24,),
+                Expanded(
+                  child: const Text(
+                    '종료일',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('2025.05.40',
+                        style: TextStyle(fontSize: 16)),
+                        Icon(Icons.calendar_today_outlined, size: 18,)
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: 24),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('2025.05.40',
+                        style: TextStyle(fontSize: 16)),
+                        Icon(Icons.calendar_today_outlined, size: 18,)
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
-        ),),
+        ),
+      ),
     );
   }
-
 }
