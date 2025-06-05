4d2d932f1e239aeb5e0ee9c452def3e20d0effcd
undercrosshd <hd907@naver.com>
2025-06-05 09:54:11 +0900

    design : 할 일 추가 버튼 추가

--- lib/features/activity_history/presentation/pages/project_detail_page/pages/project_detail_page.dart
+++ lib/features/activity_history/presentation/pages/project_detail_page/pages/project_detail_page.dart
@@ -29,10 +29,24 @@ class ProjectDetailPage extends StatelessWidget {
               height: 16,
             ),
             TeamMemberSection(), // 팀원 표시, 팀원 초대
-            ProjectTabBar(),
+            ProjectTabBar(), // 프로젝트 상세페이지 하단 탭 바
           ],
         ),
-      ),
+floatingActionButton: Container(
+  height: 50,
+  child: FloatingActionButton.extended(
+            backgroundColor: Colors.grey[700],
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(44)
+            ),
+            onPressed: () {
+              debugPrint('할 일 추가 버튼 클릭');
+            },
+            label: const Text('할 일 추가 +'),
+          ),
+),
+    )
     );
   }
 }
