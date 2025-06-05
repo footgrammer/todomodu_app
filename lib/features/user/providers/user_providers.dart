66fe4d55f9fb972ade1d147ac2fd58e410277a63
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-06-05 14:17:12 +0900

    feat: userProvider

---
+++ lib/features/user/providers/user_providers.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todomodu_app/features/user/data/datasources/user_data_source.dart';
+import 'package:todomodu_app/features/user/data/datasources/user_data_source_impl.dart';
+import 'package:todomodu_app/features/user/data/repositories/user_repository_impl.dart';
+import 'package:todomodu_app/features/user/domain/models/user_entity.dart';
+import 'package:todomodu_app/features/user/domain/repositories/user_repository.dart';
+import 'package:todomodu_app/features/user/domain/usecases/get_current_user_usecase_impl.dart';
+
+final _userDataSourceProvider = Provider<UserDataSource>((ref) {
+  return UserDataSourceImpl(
+    firebaseAuth: FirebaseAuth.instance,
+    firestore: FirebaseFirestore.instance,
+  );
+});
+
+final userRepositoryProvider = Provider<UserRepository>((ref) {
+  final userDataSource = ref.read(_userDataSourceProvider);
+  return UserRepositoryImpl(userDataSource: userDataSource);
+});
+
+final getCurrentUserUsecaseProvider = Provider<GetCurrentUserUsecaseImpl>((
+  ref,
+) {
+  final userRepository = ref.read(userRepositoryProvider);
+  return GetCurrentUserUsecaseImpl(userRepository: userRepository);
+});
+
+final userProvider = StreamProvider<UserEntity?>((ref) {
+  final usecase = ref.read(getCurrentUserUsecaseProvider);
+  return usecase.execute();
+});
