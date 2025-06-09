42b977b763782e835d6734e65314c191ad2c8bb7
K-OUZ <kywmania0908@gmail.com>
2025-06-09 16:29:41 +0900

    chore: add project_create_test_page route

--- lib/app/todomodu_app.dart
+++ lib/app/todomodu_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todomodu_app/features/ai/presentation/pages/project_create_test_page.dart';
 import 'package:todomodu_app/features/notice/presentation/pages/notice_create_page.dart';
 import 'package:todomodu_app/features/project/presentation/pages/project_detail_page.dart';
 import 'package:todomodu_app/features/project/presentation/pages/project_page.dart';
@@ -38,6 +39,11 @@ class TodomoduApp extends StatelessWidget {
                 Builder(
                   builder: (context) {
                     return moveToPage(context, ProjectDetailPage(), 'projectDetailPage');
+                  }
+                ),
+                Builder(
+                  builder: (context) {
+                    return moveToPage(context, ProjectCreateTestPage(), 'projectCreateTestPage');
                   }
                 ),
               ],
