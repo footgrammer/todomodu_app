3e843c208806c96c0e6ee71d4c27056ed7568959
K-OUZ <kywmania0908@gmail.com>
2025-06-09 14:42:03 +0900

    feat: openai_providers

---
+++ lib/features/ai/presentation/providers/openai_providers.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todomodu_app/features/ai/data/datasources/openai_data_source.dart';
+import 'package:todomodu_app/features/ai/data/datasources/openai_data_source_impl.dart';
+import 'package:todomodu_app/features/ai/data/repositories/openai_repository_impl.dart';
+import 'package:todomodu_app/features/ai/domain/models/openai_response.dart';
+import 'package:todomodu_app/features/ai/domain/repositories/openai_repository.dart';
+import 'package:todomodu_app/features/ai/domain/usecases/fetch_openai_response_usecase.dart';
+import 'package:todomodu_app/features/ai/domain/usecases/fetch_openai_response_usecase_impl.dart';
+
+final _openaiDataSourceProvider = Provider<OpenaiDataSource>((ref) {
+  return OpenaiDataSourceImpl(dio: Dio());
+});
+
+final openaiRepositoryProvider = Provider<OpenaiRepository>((ref) {
+  final openaiDataSource = ref.read(_openaiDataSourceProvider);
+  return OpenaiRepositoryImpl(openaiDataSource: openaiDataSource);
+});
+
+final openaiUsercaseProvider = Provider<FetchOpenaiResponseUsecase>((ref) {
+  final openaiRepository = ref.read(openaiRepositoryProvider);
+  return FetchOpenaiResponseUsecaseImpl(openaiRepository: openaiRepository);
+});
+
+final openaiResponseProvider = FutureProvider.autoDispose
+    .family<OpenaiResponse?, String>((ref, prompt) {
+      final openaiUsecase = ref.read(openaiUsercaseProvider);
+      return openaiUsecase.execute(prompt);
+    });
