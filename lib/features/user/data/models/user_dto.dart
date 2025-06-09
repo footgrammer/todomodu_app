f1fed7b8cd3e6d74c67780ae18c36401dddda1af
kihyun <=>
2025-06-09 15:18:45 +0900

    feat : user dto에 createdAt fromEntity 메소드 추가

--- lib/features/user/data/models/user_dto.dart
+++ lib/features/user/data/models/user_dto.dart
@@ -1,16 +1,18 @@
-import 'package:todomodu_app/features/user/domain/models/user_entity.dart';
+import 'package:todomodu_app/features/user/domain/entities/user_entity.dart';
 
 class UserDto {
   final String userId;
   final String name;
   final String profileImageUrl;
   final String email;
+  final DateTime createdAt;
 
   UserDto({
     required this.userId,
     required this.name,
     required this.profileImageUrl,
     required this.email,
+    required this.createdAt,
   });
 
   UserDto.fromJson(Map<String, dynamic> map)
@@ -19,6 +21,7 @@ class UserDto {
         name: map['name'],
         profileImageUrl: map['profileImageUrl'],
         email: map['email'],
+        createdAt: map['createdAt'],
       );
 
   Map<String, dynamic> toJson() {
@@ -27,15 +30,27 @@ class UserDto {
       'name': name,
       'profileImageUrl': profileImageUrl,
       'email': email,
+      'createdAt': createdAt,
     };
   }
 
   UserEntity toEntity() {
     return UserEntity(
-      userId: userId,
+      id: userId,
       name: name,
       profileImageUrl: profileImageUrl,
       email: email,
+      createdAt: createdAt,
+    );
+  }
+
+  factory UserDto.fromEntity(UserEntity entity) {
+    return UserDto(
+      userId: entity.id,
+      name: entity.name,
+      profileImageUrl: entity.profileImageUrl,
+      email: entity.email,
+      createdAt: entity.createdAt,
     );
   }
 }
