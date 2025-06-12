7028acc0199ab0a50aaed7d9a9399736d856f483
kihyun <=>
2025-06-12 15:04:37 +0900

    feat : notice repository 구현

--- lib/features/notice/domain/repositories/notice_repository.dart
+++ lib/features/notice/domain/repositories/notice_repository.dart
@@ -1,8 +1,22 @@
 import 'package:todomodu_app/features/notice/domain/entities/notice.dart';
+import 'package:todomodu_app/features/user/domain/entities/user_entity.dart';
 import 'package:todomodu_app/shared/types/result.dart';
 
 abstract interface class NoticeRepository {
-  Future<Result<List<Notice>>> fetchNotices();
-  Future<Result<List<Notice>>> fetchNoticesbyProjectIds(List<String> projectIds);
-  Future<Result<Notice>> fetchNoticebyId(String id);
+  Future<Result<Notice>> createNotice(Notice notice);
+
+  Future<Result<List<Notice>>> fetchNoticesbyProjectIds(
+    List<String> projectIds,
+  );
+
+  Future<Result<Notice>> fetchNoticebyId({
+    required String projectId,
+    required String noticeId,
+  });
+
+  Future<Result<Notice>> updateNotice(Notice notice);
+
+  Future<Result<Notice>> markNoticeAsRead({required Notice notice, required UserEntity user});
+
+  
 }
