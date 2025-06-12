8114e18e9d37ed202f9d1ea830fbc4bb52ddb3b5
K-OUZ <kywmania0908@gmail.com>
2025-06-12 09:57:46 +0900

    feat: 유저 프로필 이미지 출력

--- lib/features/user/presentation/widgets/profile_image.dart
+++ lib/features/user/presentation/widgets/profile_image.dart
@@ -9,50 +9,50 @@ class ProfileImage extends StatelessWidget {
   final String profileImageUrl;
   @override
   Widget build(BuildContext context) {
-    return profileImageUrl.trim().isEmpty
-        ? Stack(
-          children: [
-            Container(
+    return Stack(
+      children: [
+        profileImageUrl.trim().isEmpty
+            ? Container(
               width: 100,
               height: 100,
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 color: Colors.grey[400],
               ),
-            ),
-            Positioned(
-              right: 0,
-              bottom: 0,
-              child: GestureDetector(
-                onTap: () {
-                  log('카메라 버튼 클릭');
-                },
-                child: Container(
-                  width: 36,
-                  height: 36,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2, color: Color(0XFFF7F7F8)),
-                    shape: BoxShape.circle,
-                    color: Colors.grey[200],
-                  ),
-                  child: CustomIcon(
-                    name: 'camera',
-                    size: 18,
-                    color: Color(0XFF1C1B1F),
-                  ),
-                ),
+            )
+            : Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(shape: BoxShape.circle),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(profileImageUrl, fit: BoxFit.cover),
               ),
             ),
-          ],
-        )
-        : Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(shape: BoxShape.circle),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(100),
-            child: Image.network(profileImageUrl, fit: BoxFit.cover),
+        Positioned(
+          right: 0,
+          bottom: 0,
+          child: GestureDetector(
+            onTap: () {
+              log('카메라 버튼 클릭');
+            },
+            child: Container(
+              width: 36,
+              height: 36,
+              decoration: BoxDecoration(
+                border: Border.all(width: 2, color: Color(0XFFF7F7F8)),
+                shape: BoxShape.circle,
+                color: Colors.grey[200],
+              ),
+              child: CustomIcon(
+                name: 'camera',
+                size: 18,
+                color: Color(0XFF1C1B1F),
+              ),
+            ),
           ),
-        );
+        ),
+      ],
+    );
   }
 }
