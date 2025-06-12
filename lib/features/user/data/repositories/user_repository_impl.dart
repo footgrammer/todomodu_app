6a847df13eebcffa3ac7621439f3a331cba0d4df
K-OUZ <kywmania0908@gmail.com>
2025-06-12 18:32:11 +0900

    feat: 프로필 업로드 기능 추가

--- lib/features/user/data/repositories/user_repository_impl.dart
+++ lib/features/user/data/repositories/user_repository_impl.dart
@@ -1,4 +1,10 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
 import 'package:todomodu_app/features/user/data/datasources/user_data_source.dart';
 import 'package:todomodu_app/features/user/domain/models/user_entity.dart';
 import 'package:todomodu_app/features/user/domain/repositories/user_repository.dart';
@@ -21,5 +27,26 @@ class UserRepositoryImpl implements UserRepository {
   @override
   Future<void> changeUserNickname(String userId, String nickname) async {
     await userRef.doc(userId).update({'name': nickname});
+  }
+
+  @override
+  Future<void> uploadProfileImage(String userId) async {
+    final picker = ImagePicker();
+    final storage = FirebaseStorage.instance;
+
+    final XFile? pickedFile = await picker.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    File file = File(pickedFile!.path);
+
+    try {
+      final ref = storage.ref().child('profile_images/$userId');
+      final uploadTask = await ref.putFile(file);
+      final downloadUrl = await uploadTask.ref.getDownloadURL();
+      await userRef.doc(userId).update({'profileImageUrl': downloadUrl});
+    } catch (e) {
+      log('파일 업로드 실패: $e');
+    }
   }
 }
--- lib/features/user/domain/repositories/user_repository.dart
+++ lib/features/user/domain/repositories/user_repository.dart
@@ -3,4 +3,5 @@ import 'package:todomodu_app/features/user/domain/models/user_entity.dart';
 abstract interface class UserRepository {
   Stream<UserEntity?> getCurrentUser();
   Future<void> changeUserNickname(String userId, String nickname);
+  Future<void> uploadProfileImage(String userId);
 }
