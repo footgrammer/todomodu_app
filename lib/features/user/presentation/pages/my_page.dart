8a2b93a436b3f380e91dc555267e01868e556cd7
K-OUZ <kywmania0908@gmail.com>
2025-06-12 19:40:05 +0900

    design: 로그아웃 시 자연스러운 화면 전환

--- lib/features/user/presentation/pages/my_page.dart
+++ lib/features/user/presentation/pages/my_page.dart
@@ -21,123 +21,130 @@ class MyPage extends ConsumerWidget {
       loading: () => const Center(child: CircularProgressIndicator()),
       error: (error, stack) => Center(child: Text('$error')),
       data: (user) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              '마이',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            actions: [
-              IconButton(
-                onPressed: () {
-                  log('알림 버튼 클릭');
-                },
-                icon: const CustomIcon(name: 'bell'),
-              ),
-            ],
-          ),
-          body: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      ProfileImage(user: user!),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 4, 4, 0),
-                        child: Text(user.name, style: TextStyle(fontSize: 22)),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          log('닉네임 변경 버튼 클릭');
-                          showDialog(
-                            context: context,
-                            builder:
-                                (context) =>
-                                    EditNicknameDialog(userId: user.userId),
-                          );
-                        },
-                        child: Container(
-                          width: 28,
-                          height: 28,
-                          color: Colors.transparent,
-                          child: CustomIcon(
-                            name: 'Edit_Pencil_Line_01',
-                            size: 18,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 18),
-                  CustomMenuBar(
-                    text: '종료된 프로젝트',
+        return user == null
+            ? Scaffold(
+              appBar: AppBar(),
+            ) // 자연스러운 화면 이동을 위해 추가
+            : Scaffold(
+              appBar: AppBar(
+                title: const Text(
+                  '마이',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                actions: [
+                  IconButton(
                     onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ClosedProjectListPage(),
-                        ),
-                      );
+                      log('알림 버튼 클릭');
                     },
-                  ),
-                  CustomMenuBar(
-                    text: '알림 설정',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => NotificationSettingsPage(),
-                        ),
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 18),
-                  CustomMenuBar(
-                    text: '이용약관 및 개인정보 처리방침',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => TermsAndPrivacyPage(),
-                        ),
-                      );
-                    },
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('버전 정보', style: TextStyle(fontSize: 18)),
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8),
-                          child: Text(
-                            '1.0.0',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  CustomMenuBar(
-                    text: '로그아웃',
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => LogoutDialog(),
-                      );
-                    },
+                    icon: const CustomIcon(name: 'bell'),
                   ),
                 ],
               ),
-            ),
-          ),
-        );
+              body: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          ProfileImage(user: user),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(20, 4, 4, 0),
+                            child: Text(
+                              user.name,
+                              style: TextStyle(fontSize: 22),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              log('닉네임 변경 버튼 클릭');
+                              showDialog(
+                                context: context,
+                                builder:
+                                    (context) =>
+                                        EditNicknameDialog(userId: user.userId),
+                              );
+                            },
+                            child: Container(
+                              width: 28,
+                              height: 28,
+                              color: Colors.transparent,
+                              child: CustomIcon(
+                                name: 'Edit_Pencil_Line_01',
+                                size: 18,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 18),
+                      CustomMenuBar(
+                        text: '종료된 프로젝트',
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ClosedProjectListPage(),
+                            ),
+                          );
+                        },
+                      ),
+                      CustomMenuBar(
+                        text: '알림 설정',
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => NotificationSettingsPage(),
+                            ),
+                          );
+                        },
+                      ),
+                      const SizedBox(height: 18),
+                      CustomMenuBar(
+                        text: '이용약관 및 개인정보 처리방침',
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TermsAndPrivacyPage(),
+                            ),
+                          );
+                        },
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('버전 정보', style: TextStyle(fontSize: 18)),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 8),
+                              child: Text(
+                                '1.0.0',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      CustomMenuBar(
+                        text: '로그아웃',
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) => LogoutDialog(),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
       },
     );
   }
