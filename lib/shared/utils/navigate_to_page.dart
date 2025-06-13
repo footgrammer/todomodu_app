5460a5514d50dc6d522366b5e79dc4ab8fdec288
K-OUZ <kywmania0908@gmail.com>
2025-06-13 15:57:57 +0900

    chore: replaceWithPage 함수 재사용

--- lib/shared/utils/navigate_to_page.dart
+++ lib/shared/utils/navigate_to_page.dart
@@ -3,3 +3,10 @@ import 'package:flutter/material.dart';
 void navigateToPage(BuildContext context, Widget page) {
   Navigator.push(context, MaterialPageRoute(builder: (context) => page));
 }
+
+void replaceWithPage(BuildContext context, Widget page) {
+  Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(builder: (context) => page),
+  );
+}
