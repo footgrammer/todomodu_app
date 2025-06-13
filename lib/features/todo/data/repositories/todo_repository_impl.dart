b4454ee48df23ccb8dd6574469112acdebebe4ce
undercrosshd <hd907@naver.com>
2025-06-13 14:57:22 +0900

    refactor: todo_repository_impl 구현 추가
    repository 확장

--- lib/features/todo/data/repositories/todo_repository_impl.dart
+++ lib/features/todo/data/repositories/todo_repository_impl.dart
@@ -2,13 +2,18 @@ import 'package:todomodu_app/features/todo/data/datasources/todo_remote_datasour
 import 'package:todomodu_app/features/todo/domain/entities/todo.dart';
 import 'package:todomodu_app/features/todo/domain/repositories/todo_repository.dart';
 
-class TodoRepositoryImpl implements TodoRepository{
+class TodoRepositoryImpl implements TodoRepository {
   final TodoRemoteDatasource remoteDatasource;
-  
+
   TodoRepositoryImpl(this.remoteDatasource);
 
   @override
   Future<void> createTodo(Todo todo) async {
     await remoteDatasource.createTodo(todo);
+  }
+
+  @override
+  Future<List<Todo>> fetchTodos() async {
+    return await remoteDatasource.fetchTodos();
   }
 }
