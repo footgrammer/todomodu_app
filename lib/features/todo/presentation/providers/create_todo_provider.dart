4bd49cc7847d0d5857e77b904bf554ba9b84f0cb
undercrosshd <hd907@naver.com>
2025-06-13 13:50:25 +0900

    refactor: create_todo_provider 코드 수정

--- lib/features/todo/presentation/providers/create_todo_provider.dart
+++ lib/features/todo/presentation/providers/create_todo_provider.dart
@@ -1,12 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todomodu_app/features/todo/application/usecases/create_todo_usecase.dart';
-import 'package:todomodu_app/features/todo/data/repositories/todo_repository_impl.dart';
+import 'package:todomodu_app/features/todo/presentation/providers/todo_repository_provider.dart';
 
-final todoRepositoryProvider = Provider<TodoRepositoryImpl>((ref) {
-  return TodoRepositoryImpl();
-});
-
-final Provider<CreateTodoUseCase> createTodoUseCaseProvider = Provider((ref) {
+final createTodoUseCaseProvider = Provider<CreateTodoUseCase>((ref) {
   final repository = ref.read(todoRepositoryProvider);
   return CreateTodoUseCase(repository);
 });
