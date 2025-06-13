8553ade244d867b6005ac18912db10974c650124
undercrosshd <hd907@naver.com>
2025-06-13 14:51:39 +0900

    refactor: todo_remote_datasource.dart 수정

--- lib/features/todo/data/datasources/todo_remote_datasource.dart
+++ lib/features/todo/data/datasources/todo_remote_datasource.dart
@@ -8,22 +8,30 @@ class TodoRemoteDatasource {
   TodoRemoteDatasource(this.firestore);
 
   Future<void> createTodo(Todo todo) async {
-    final todoDoc = firestore.collection('todos').doc(todo.id);
-
-    await todoDoc.set({
+    await firestore.collection('todos').add({
       'projectId': todo.projectId,
       'title': todo.title,
       'startDate': todo.startDate.toIso8601String(),
       'endDate': todo.endDate.toIso8601String(),
       'isDone': todo.isDone,
+      'subTasks': [],
     });
+  }
 
-    for (final subTask in todo.subTasks) {
-      final subTaskDoc = todoDoc.collection('subTasks').doc(subTask.id);
-      await subTaskDoc.set({
-        'title': subTask.title,
-        'isDone': subTask.isDone,
-      });
+    Future<List<Todo>> fetchTodos() async {
+      final snapshot = await firestore.collection('todos').get();
+
+      return snapshot.docs.map((doc) {
+        final data = doc.data();
+        return Todo(
+          id: doc.id,
+          projectId: data['projectId'],
+          title: data['title'],
+          startDate: DateTime.parse(data['startDate']),
+          endDate: DateTime.parse(data['endDate']),
+          isDone: data['isDone'],
+          subTasks: [],
+        );
+      }).toList();
     }
   }
-}
