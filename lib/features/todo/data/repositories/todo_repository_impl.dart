bc30367993c0588c20bebee24c3f4fb0b04a5484
undercrosshd <hd907@naver.com>
2025-06-13 13:23:46 +0900

    refactor: todo_repository_impl 수정
    remoteDataSource에게 저장을 위임

--- lib/features/todo/data/repositories/todo_repository_impl.dart
+++ lib/features/todo/data/repositories/todo_repository_impl.dart
@@ -1,17 +1,14 @@
+import 'package:todomodu_app/features/todo/data/datasources/todo_remote_datasource.dart';
 import 'package:todomodu_app/features/todo/domain/entities/todo.dart';
 import 'package:todomodu_app/features/todo/domain/repositories/todo_repository.dart';
 
 class TodoRepositoryImpl implements TodoRepository{
-  final List<Todo> _storage = []; //임시 디비?
+  final TodoRemoteDatasource remoteDatasource;
+  
+  TodoRepositoryImpl(this.remoteDatasource);
 
   @override
   Future<void> createTodo(Todo todo) async {
-    _storage.add(todo);
-  }
-
-
-// 리스트 확인 메소드(테스트용)
-  Future<List<Todo>> getAllTodos() async {
-    return _storage;
+    await remoteDatasource.createTodo(todo);
   }
 }
