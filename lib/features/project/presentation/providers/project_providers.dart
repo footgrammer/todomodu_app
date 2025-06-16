2f76e02b37ca77624e813dcf51db1bf187e0cdf0
kihyun <=>
2025-06-16 15:50:07 +0900

    feat : project provider 추가

---
+++ lib/features/project/presentation/providers/project_providers.dart
@@ -0,0 +1,19 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todomodu_app/features/project/data/datasources/project_data_source_impl.dart';
+import 'package:todomodu_app/features/project/data/repositories/project_repository_impl.dart';
+import 'package:todomodu_app/features/project/domain/repositories/project_repository.dart';
+import 'package:todomodu_app/features/project/domain/usecases/fetch_projects_by_user_usecase.dart';
+
+final _projectDataSource = Provider<ProjectDataSourceImpl>((ref){
+  return ProjectDataSourceImpl(firestore: FirebaseFirestore.instance);
+});
+
+final projectRepository = Provider<ProjectRepository>((ref) {
+  return ProjectRepositoryImpl(dataSource: ref.watch(_projectDataSource));
+},);
+
+final fetchProjectsByUserUsecase = Provider<FetchProjectsByUserUsecase>((ref) {
+  return FetchProjectsByUserUsecase(projectRepository: ref.watch(projectRepository));
+},);
