04233ef8dc5e99e14b9caf7cf8cf1e72eb2d0246
undercrosshd <hd907@naver.com>
2025-06-18 02:57:48 +0900

    refactor: todo_repository 수정
    deleteTodo, toggleSubtaskDone도 projectId 받음
    streamTodos가 projectId를 파라미터로 받게 변경됨

--- lib/features/todo/domain/repositories/todo_repository.dart
+++ lib/features/todo/domain/repositories/todo_repository.dart
@@ -2,12 +2,13 @@ import 'package:todomodu_app/features/todo/domain/entities/todo.dart';
 
 abstract class TodoRepository {
   Future<void> createTodo(Todo todo);
-  Stream<List<Todo>> streamTodos();
-  Future<void> deleteTodo(String todoId);
+  Stream<List<Todo>> streamTodos(String projectId);
+  Future<void> deleteTodo(String projectId, String todoId);
 
-  Future<void> toggleSubTaskDone({
+  Future<void> toggleSubtaskDone({
+    required String projectId,
     required String todoId,
-    required String subTaskId,
+    required String subtaskId,
     required bool isDone,
   });
 
