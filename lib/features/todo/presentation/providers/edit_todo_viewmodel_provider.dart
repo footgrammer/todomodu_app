8fa3727a7e0e29c91fff02b6951ce57a4d97aae8
undercrosshd <hd907@naver.com>
2025-06-18 02:00:54 +0900

    refactor: edit_todo_viewmodel_provider.dart 수정

--- lib/features/todo/presentation/providers/edit_todo_viewmodel_provider.dart
+++ lib/features/todo/presentation/providers/edit_todo_viewmodel_provider.dart
@@ -1,12 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../domain/entities/todo.dart';
 import '../viewmodels/edit_todo_viewmodel.dart';
+import '../states/edit_todo_state.dart';
 import 'update_todo_usecase_provider.dart';
 
-final editTodoViewModelProvider = Provider.family<EditTodoViewModel, Todo>((ref, todo) {
-  final updateTodoUseCase = ref.watch(updateTodoUseCaseProvider);
-  return EditTodoViewModel.create(
-    todo: todo,
-    updateTodoUseCase: updateTodoUseCase,
-  );
-});
+final editTodoViewModelProvider = StateNotifierProvider.family<EditTodoViewModel, EditTodoState, Todo>(
+  (ref, todo) {
+    final updateTodoUseCase = ref.watch(updateTodoUseCaseProvider);
+    return EditTodoViewModel(
+      todo: todo,
+      updateTodoUseCase: updateTodoUseCase,
+    );
+  },
+);
