651e2aadffafe8ed036d259357214b6fbb2de66b
undercrosshd <hd907@naver.com>
2025-06-18 02:53:03 +0900

    refactor: todo.dart 수정

--- lib/features/todo/domain/entities/todo.dart
+++ lib/features/todo/domain/entities/todo.dart
@@ -1,21 +1,46 @@
-import 'package:todomodu_app/features/todo/domain/entities/sub_task.dart';
+import 'subtask.dart';
 
 class Todo {
-  String id;
-  String projectId;
-  String title;
-  List<SubTask> subTasks;
-  DateTime startDate;
-  DateTime endDate;
-  bool isDone;
+  final String id;
+  final String projectId;
+  final String title;
+  final List<Subtask> subtasks;
+  final DateTime startDate;
+  final DateTime endDate;
+  final bool isDone;
 
   Todo({
     required this.id,
     required this.projectId,
     required this.title,
-    required this.subTasks,
+    required this.subtasks,
     required this.startDate,
     required this.endDate,
     required this.isDone,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'projectId': projectId,
+      'title': title,
+      'startDate': startDate.toIso8601String(),
+      'endDate': endDate.toIso8601String(),
+      'isDone': isDone,
+      'subtasks': subtasks.map((e) => e.toMap()).toList(),
+    };
+  }
+
+  factory Todo.fromMap(String id, Map<String, dynamic> map) {
+    return Todo(
+      id: id,
+      projectId: map['projectId'],
+      title: map['title'],
+      startDate: DateTime.parse(map['startDate']),
+      endDate: DateTime.parse(map['endDate']),
+      isDone: map['isDone'],
+      subtasks: (map['subtasks'] as List<dynamic>)
+          .map((e) => Subtask.fromMap(e as Map<String, dynamic>))
+          .toList(),
+    );
+  }
 }
