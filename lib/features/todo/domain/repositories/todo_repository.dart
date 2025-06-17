3527976c8a4cf029d1002c35e3fa225128bcd302
undercrosshd <hd907@naver.com>
2025-06-18 00:23:19 +0900

    feat: todo_repository.dart 할 일 삭제 기능 추가

--- lib/features/todo/domain/repositories/todo_repository.dart
+++ lib/features/todo/domain/repositories/todo_repository.dart
@@ -3,4 +3,5 @@ import 'package:todomodu_app/features/todo/domain/entities/todo.dart';
 abstract class TodoRepository {
   Future<void> createTodo(Todo todo);
   Stream<List<Todo>> streamTodos();
+  Future<void> deleteTodo(String todoId);
 }
