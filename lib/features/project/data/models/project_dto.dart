b6b24421309ebbb0e2136b30da0d5879760dd575
kihyun <=>
2025-06-18 00:05:06 +0900

    fix : project와 projectDto이름을 firebase와 맞추기

--- lib/features/project/data/models/project_dto.dart
+++ lib/features/project/data/models/project_dto.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:todomodu_app/features/project/domain/entities/project.dart';
 import 'package:todomodu_app/features/todo/domain/entities/todo.dart';
 import 'package:todomodu_app/features/user/domain/entities/user_entity.dart';
@@ -6,24 +7,24 @@ class ProjectDto {
   String id;
   String title;
   String description;
-  DateTime startTime;
-  DateTime endTime;
+  DateTime startDate;
+  DateTime endDate;
   String ownerId;
   // List<String> memberIds;
   // List<String> todoIds;
-  String inviteCode;
+  String invitationCode;
   bool isDone;
 
   ProjectDto({
     required this.id,
     required this.title,
     required this.description,
-    required this.startTime,
-    required this.endTime,
+    required this.startDate,
+    required this.endDate,
     required this.ownerId,
     // required this.memberIds,
     // required this.todoIds,
-    required this.inviteCode,
+    required this.invitationCode,
     required this.isDone,
   });
 
@@ -32,12 +33,13 @@ class ProjectDto {
       id: json['id'] as String,
       title: json['title'] as String,
       description: json['description'] as String,
-      startTime: DateTime.parse(json['startTime']),
-      endTime: DateTime.parse(json['endTime']),
+      // Timestamp → DateTime
+      startDate: (json['startDate'] as Timestamp).toDate(),
+      endDate: (json['endDate'] as Timestamp).toDate(),
       ownerId: json['ownerId'] as String,
       // memberIds: List<String>.from(json['memberIds'] ?? []),
-      // todoIds: List<String>.from(json['todoIds'] ?? []),
-      inviteCode: json['inviteCode'] as String,
+      // todoIds:   List<String>.from(json['todoIds']   ?? []),
+      invitationCode: json['invitationCode'] as String,
       isDone: json['isDone'] as bool,
     );
   }
@@ -47,12 +49,13 @@ class ProjectDto {
       'id': id,
       'title': title,
       'description': description,
-      'startTime': startTime.toIso8601String(),
-      'endTime': endTime.toIso8601String(),
+      // DateTime → Timestamp
+      'startDate': Timestamp.fromDate(startDate),
+      'endDate': Timestamp.fromDate(endDate),
       'ownerId': ownerId,
       // 'memberIds': memberIds,
-      // 'todoIds': todoIds,
-      'inviteCode': inviteCode,
+      // 'todoIds':   todoIds,
+      'inviteCode': invitationCode,
       'isDone': isDone,
     };
   }
@@ -62,12 +65,12 @@ class ProjectDto {
       id: entity.id,
       title: entity.title,
       description: entity.description,
-      startTime: entity.startTime,
-      endTime: entity.endTime,
+      startDate: entity.startDate,
+      endDate: entity.endDate,
       ownerId: entity.owner.id,
       // memberIds: entity.members.map((e) => e.id).toList(),
-      // todoIds: entity.todos.map((e) => e.id).toList(),
-      inviteCode: entity.inviteCode,
+      // todoIds:   entity.todos.map((e) => e.id).toList(),
+      invitationCode: entity.invitationCode,
       isDone: entity.isDone,
     );
   }
@@ -81,12 +84,12 @@ class ProjectDto {
       id: id,
       title: title,
       description: description,
-      startTime: startTime,
-      endTime: endTime,
+      startDate: startDate,
+      endDate: endDate,
       owner: owner,
       members: members,
       todos: todos,
-      inviteCode: inviteCode,
+      invitationCode: invitationCode,
       isDone: isDone,
     );
   }
