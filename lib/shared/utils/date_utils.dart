954a0f830bf2ff634f005b886576c8fb46ee5478
kihyun <=>
2025-06-18 23:41:27 +0900

    feat : 공지 리스트에 필요한 util추가

--- lib/shared/utils/date_utils.dart
+++ lib/shared/utils/date_utils.dart
@@ -9,3 +9,8 @@ String formatKoreanDate(DateTime? date) {
   if (date == null) return '';
   return DateFormat('yyyy년 MM월 dd일', 'ko').format(date);
 }
+
+String formatDateDottedYMD(DateTime? date) {
+  if (date == null) return '';
+  return DateFormat('yyyy.MM.dd').format(date);
+}
