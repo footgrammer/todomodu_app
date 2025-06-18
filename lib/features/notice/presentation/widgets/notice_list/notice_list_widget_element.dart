d59a720943a39e6efd6668514b2af24dc83d2601
kihyun <=>
2025-06-18 23:41:41 +0900

    design : 공지 생성 페이지 디자인

--- lib/features/notice/presentation/widgets/notice_list/notice_list_widget_element.dart
+++ lib/features/notice/presentation/widgets/notice_list/notice_list_widget_element.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:todomodu_app/features/notice/domain/entities/notice.dart';
 import 'package:todomodu_app/features/notice/presentation/widgets/notice_list/expanded_text_box/expanded_text_widget.dart';
+import 'package:todomodu_app/shared/constants/app_colors.dart';
+import 'package:todomodu_app/shared/utils/color_utils.dart';
+import 'package:todomodu_app/shared/utils/date_utils.dart';
 
 class NoticeListWidgetElement extends StatelessWidget {
   const NoticeListWidgetElement({required this.notice, super.key});
@@ -9,10 +12,11 @@ class NoticeListWidgetElement extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
       decoration: BoxDecoration(
+        color : brightPastelFromSeed(notice.projectId),
         borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: Colors.black),
+        // border: Border.all(color: AppColors.grey600),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -20,6 +24,10 @@ class NoticeListWidgetElement extends StatelessWidget {
           Text(
             notice.title,
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            formatDateDottedYMD(notice.createdAt),
+            style: Theme.of(context).textTheme.bodySmall?.copyWith(color: AppColors.grey600),
           ),
           SizedBox(height: 8),
           ExpandedTextWidget(content: notice.content),
--- lib/features/notice/presentation/widgets/project_chip.dart
+++ lib/features/notice/presentation/widgets/project_chip.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todomodu_app/features/project/domain/entities/project.dart';
-
+import 'package:todomodu_app/shared/constants/app_colors.dart';
+import 'package:todomodu_app/shared/utils/color_utils.dart';
 
 class ProjectChip extends StatelessWidget {
-  const ProjectChip({required this.isChecked, required this.project, super.key});
+  const ProjectChip({
+    required this.isChecked,
+    required this.project,
+    super.key,
+  });
 
   final bool isChecked;
   final Project project;
@@ -13,12 +18,20 @@ class ProjectChip extends StatelessWidget {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10),
       decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black
-        ),
-        borderRadius: BorderRadius.circular(100)
+        color: brightPastelFromSeed(project.id),
+        // border: Border.all(
+        //   color: Colors.black
+        // ),
+        borderRadius: BorderRadius.circular(100),
       ),
-      child: Center(child: Text(project.title)),
+      child: Center(
+        child: Text(
+          project.title,
+          style: Theme.of(
+            context,
+          ).textTheme.bodySmall,
+        ),
+      ),
     );
   }
 }
