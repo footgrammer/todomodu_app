b80e37b72512443cd77da7bdbbaef63a4c54fdda
undercrosshd <hd907@naver.com>
2025-06-18 10:54:56 +0900

    feat: 할 일 추가/수정 페이지에서 할 일 이름 필수 입력 받기, 경고 메시지 기능, 최대 글자수 구현

--- lib/features/todo/presentation/widgets/todo_title_input.dart
+++ lib/features/todo/presentation/widgets/todo_title_input.dart
@@ -2,39 +2,37 @@ import 'package:flutter/material.dart';
 
 class TodoTitleInput extends StatelessWidget {
   final TextEditingController controller;
-  final void Function(String)? onChanged; 
+  final void Function(String)? onChanged;
+  final String? errorText;
 
   const TodoTitleInput({
     super.key,
     required this.controller,
     this.onChanged,
+    this.errorText,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          '할 일 이름',
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: 8),
-        TextField(
-          controller: controller,
-          onChanged: onChanged,
-          decoration: InputDecoration(
-            hintText: '할 일 1',
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-                borderSide: BorderSide.none),
-            contentPadding:
-                const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
-            filled: true,
-            fillColor: Colors.grey[300],
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      const Text('할 일 이름', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+      const SizedBox(height: 8),
+      TextField(
+        controller: controller,
+        onChanged: onChanged,
+        maxLength: 30,
+        decoration: InputDecoration(
+          hintText: errorText ?? '할 일 제목을 입력하세요',
+          hintStyle: TextStyle(color: errorText != null ? Colors.red : Colors.grey),
+          errorText: null,
+          counterText: '',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide.none,
           ),
+          filled: true, fillColor: Colors.grey[300],
         ),
-      ],
-    );
+      ),
+    ]);
   }
 }
