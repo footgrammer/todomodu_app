5e057639711c455e37552d6ccd403fbbb9475597
K-OUZ <kywmania0908@gmail.com>
2025-06-23 11:03:10 +0900

    design: 폰크 크기, 간격, 컬러 수정 및 컨테이너 크기 변경

--- lib/features/user/presentation/widgets/logout_dialog.dart
+++ lib/features/user/presentation/widgets/logout_dialog.dart
@@ -10,42 +10,76 @@ class LogoutDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      backgroundColor: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.only(top: 32),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '로그아웃',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: Text('로그아웃 하시겠습니까?'),
-            ),
-            Consumer(
-              builder: (BuildContext context, WidgetRef ref, Widget? child) {
-                return DualActionButtons(
-                  onCancel: () {
-                    Navigator.pop(context);
-                  },
-                  onConfirm: () async {
-                    final authRepo = ref.read(authProvider);
-                    await authRepo.signOut();
-                    ref.invalidate(userProvider);
-                    Navigator.of(context)
-                      ..pop()
-                      ..pushReplacement(
-                        MaterialPageRoute(builder: (context) => LoginPage()),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24),
+            color: Colors.white,
+          ),
+          child: SizedBox(
+            width: 327,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '로그아웃',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF403F4B),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    child: Text(
+                      '로그아웃 하시겠습니까?',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF403F4B),
+                        fontFamily: 'Pretendard',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        height: 1.5,
+                        letterSpacing: -0.32,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Consumer(
+                    builder: (
+                      BuildContext context,
+                      WidgetRef ref,
+                      Widget? child,
+                    ) {
+                      return DualActionButtons(
+                        onCancel: () {
+                          Navigator.pop(context);
+                        },
+                        onConfirm: () async {
+                          final authRepo = ref.read(authProvider);
+                          await authRepo.signOut();
+                          ref.invalidate(userProvider);
+                          Navigator.of(context)
+                            ..pop()
+                            ..pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => LoginPage(),
+                              ),
+                            );
+                        },
                       );
-                  },
-                );
-              },
+                    },
+                  ),
+                ],
+              ),
             ),
-          ],
+          ),
         ),
       ),
     );
