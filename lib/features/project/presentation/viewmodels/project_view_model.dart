16e81caf6626229f83421783909b310d0181847c
footballlover <junseok0211@nsu.ac.kr>
2025-06-24 01:15:47 +0900

    chagne : project_dto.dart

--- lib/features/project/presentation/viewmodels/project_view_model.dart
+++ lib/features/project/presentation/viewmodels/project_view_model.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todomodu_app/features/project/data/models/Project.dart';
-import 'package:todomodu_app/features/project/presentation/models/project_state.dart';
+import 'package:todomodu_app/features/project/presentation/models/project_list_state.dart';
 
 // 상태 클래스 관리할 뷰모델
 class ProjectViewModel extends Notifier<ProjectState> {
