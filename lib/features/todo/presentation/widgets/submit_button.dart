47589e4e546d53462987fd205eb942bbd99d1a2e
undercrosshd <hd907@naver.com>
2025-06-24 07:55:03 +0900

    feat: 할 일 추가/수정 페이지 제출 버튼 활성화/비활성화 기능 추가

--- lib/features/todo/presentation/widgets/submit_button.dart
+++ lib/features/todo/presentation/widgets/submit_button.dart
@@ -4,28 +4,36 @@ import 'package:todomodu_app/shared/themes/app_theme.dart';
 class SubmitButton extends StatelessWidget {
   final String label;
   final VoidCallback onPressed;
+  final bool enabled; 
 
-  const SubmitButton({required this.label, required this.onPressed, super.key});
+  const SubmitButton({
+    required this.label,
+    required this.onPressed,
+    this.enabled = false,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.fromLTRB(24, 24, 24, 58),
+      padding: const EdgeInsets.fromLTRB(24, 24, 24, 58),
       child: SizedBox(
         width: double.infinity,
         height: 56,
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
-            backgroundColor: AppColors.grey100,
-
+            backgroundColor:
+                enabled ? AppColors.primary600 : AppColors.grey100,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12),
             ),
           ),
-          onPressed: onPressed,
+          onPressed: enabled ? onPressed : null,
           child: Text(
             label,
-            style: AppTextStyles.subtitle1.copyWith(color: AppColors.grey400),
+            style: AppTextStyles.subtitle1.copyWith(
+              color: enabled ? Colors.white : AppColors.grey400,
+            ),
           ),
         ),
       ),
