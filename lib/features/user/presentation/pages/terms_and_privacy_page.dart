65d351c7647c1825520caf60578fc8b1edea4477
K-OUZ <kywmania0908@gmail.com>
2025-06-23 10:34:00 +0900

    design: 텍스트 크기, 간격 수정

--- lib/features/user/presentation/pages/terms_and_privacy_page.dart
+++ lib/features/user/presentation/pages/terms_and_privacy_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:todomodu_app/shared/themes/app_theme.dart';
 
 class TermsAndPrivacyPage extends StatefulWidget {
   const TermsAndPrivacyPage({super.key});
@@ -26,23 +27,33 @@ class _TermsAndPrivacyPageState extends State<TermsAndPrivacyPage> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        title: const Text(
+        title: Text(
           '이용약관 및 개인정보 처리방침',
-          style: TextStyle(fontWeight: FontWeight.bold),
+          style: AppTextStyles.header3.copyWith(
+            color: theme.colorScheme.onSurface,
+          ),
         ),
       ),
-      body: termsText.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-              padding: const EdgeInsets.all(16),
-              child: Text(
-                termsText,
-                style: const TextStyle(fontSize: 14, height: 1.6),
+      body:
+          termsText.isEmpty
+              ? const Center(child: CircularProgressIndicator())
+              : SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 16,
+                ),
+                child: Text(
+                  termsText,
+                  style: AppTextStyles.body2.copyWith(
+                    color: theme.colorScheme.onSurface,
+                  ),
+                ),
               ),
-            ),
     );
   }
 }
