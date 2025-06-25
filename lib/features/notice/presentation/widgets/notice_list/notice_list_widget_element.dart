ff9f2b69f839ee8a54e22fe58d4517ba8214baf6
kihyun <=>
2025-06-25 23:56:11 +0900

    [notice list page] 공지 페이지 미비점 보완 Fixes #71

--- lib/features/notice/presentation/widgets/notice_list/notice_list_widget_element.dart
+++ lib/features/notice/presentation/widgets/notice_list/notice_list_widget_element.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todomodu_app/features/notice/domain/entities/notice.dart';
+import 'package:todomodu_app/features/notice/presentation/providers/notice_providers.dart';
 import 'package:todomodu_app/features/notice/presentation/widgets/notice_list/red_dot.dart';
 import 'package:todomodu_app/features/user/domain/entities/user_entity.dart';
 import 'package:todomodu_app/shared/constants/app_colors.dart';
@@ -21,33 +23,38 @@ class NoticeListWidgetElement extends StatelessWidget {
     final isUnread =
         !notice.checkedUsers.any((user) => user.userId == currentUser.userId);
 
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-      decoration: BoxDecoration(
-        color: brightPastelFromSeed(notice.projectId),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return Consumer(
+      builder: (context, ref, child) {
+        final noticeListVm = ref.watch(noticeListViewModelProvider.notifier);
+        return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          decoration: BoxDecoration(
+            color: noticeListVm.getColorByNotice(notice),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Stack(
             children: [
-              Text(
-                notice.title,
-                style: Theme.of(context).textTheme.displaySmall,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    notice.title,
+                    style: Theme.of(context).textTheme.displaySmall,
+                  ),
+                  Text(
+                    formatDateDottedYMD(notice.createdAt),
+                    style: Theme.of(
+                      context,
+                    ).textTheme.bodySmall?.copyWith(color: AppColors.grey600),
+                  ),
+                ],
               ),
-              Text(
-                formatDateDottedYMD(notice.createdAt),
-                style: Theme.of(
-                  context,
-                ).textTheme.bodySmall?.copyWith(color: AppColors.grey600),
-              ),
+              if (isUnread)
+                const Align(alignment: Alignment.topRight, child: RedDot()),
             ],
           ),
-          if (isUnread)
-            const Align(alignment: Alignment.topRight, child: RedDot()),
-        ],
-      ),
+        );
+      }
     );
   }
 }
