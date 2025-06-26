250c36240c8f7260846d6e1dd12032e4d96796ed
K-OUZ <kywmania0908@gmail.com>
2025-06-26 12:25:03 +0900

    feat: 종료된 프로젝트 확인 기능

--- lib/features/user/presentation/pages/closed_project_list_page.dart
+++ lib/features/user/presentation/pages/closed_project_list_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todomodu_app/features/project/presentation/providers/project_providers.dart';
+import 'package:todomodu_app/features/project/presentation/widgets/project/project_card_list.dart';
 
-class ClosedProjectListPage extends StatelessWidget {
+class ClosedProjectListPage extends ConsumerWidget {
   const ClosedProjectListPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(projectListViewModelProvider);
+    final viewModel = ref.read(projectListViewModelProvider.notifier);
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
@@ -13,7 +18,32 @@ class ClosedProjectListPage extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      body: Center(child: Text('개발중인 기능입니다.')),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: RefreshIndicator(
+          onRefresh: () async {
+            await viewModel.fetchProjectsByUserId();
+          },
+          child: IgnorePointer(
+            child: Center(
+              child: Column(
+                // ProjectCardList가 Expanded로 감싸져있어 Column 추가했습니다
+                children: [
+                  ProjectCardList(
+                    projects:
+                        state.projects!
+                            .where((project) => project.isDone == true)
+                            .toList(),
+                    titleText: '아직 종료된 프로젝트가 없습니다.',
+                    subText: '프로젝트를 완료하고\n목록에서 확인해보세요!',
+                    image: 'assets/images/closed_project_empty_img.svg',
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
