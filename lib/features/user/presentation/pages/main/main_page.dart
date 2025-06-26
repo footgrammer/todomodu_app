01db1084a23265f8e576b50f3f85232a012c25e7
kihyun <=>
2025-06-26 15:14:03 +0900

    chore : Notice CreatePage에서 NoticeList페이지로 변경

--- lib/features/user/presentation/pages/main/main_page.dart
+++ lib/features/user/presentation/pages/main/main_page.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:todomodu_app/features/notice/presentation/pages/notice_create_page.dart';
 import 'package:todomodu_app/features/notice/presentation/pages/notice_list_page.dart';
-import 'package:todomodu_app/features/project/presentation/pages/project_detail_page.dart';
 import 'package:todomodu_app/features/project/presentation/pages/project_list_page.dart';
-import 'package:todomodu_app/features/todo/presentation/pages/add_todo_page.dart';
-import 'package:todomodu_app/features/todo/presentation/pages/todo_detail_page.dart';
 import 'package:todomodu_app/features/todo/presentation/pages/todo_page.dart';
 import 'package:todomodu_app/features/user/presentation/pages/my_page.dart';
 import 'package:todomodu_app/shared/constants/app_colors.dart';
@@ -27,7 +23,7 @@ class _MainPageState extends State<MainPage> {
         children: [
           ProjectListPage(),
           TodoPage(),
-          NoticeCreatePage(projectId: '1'),
+          NoticeListPage(),
           MyPage(),
         ],
       ),
