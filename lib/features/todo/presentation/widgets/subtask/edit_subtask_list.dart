75f6d0c62a74f07e9d5affea2cf0f239f83b4b5d
undercrosshd <hd907@naver.com>
2025-06-27 14:07:36 +0900

    feat: EditSubtaskList가 Firestore stream 대신 ViewModel 상태 사용하도록 기능 추가

--- lib/features/todo/presentation/widgets/subtask/edit_subtask_list.dart
+++ lib/features/todo/presentation/widgets/subtask/edit_subtask_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todomodu_app/features/todo/domain/entities/todo.dart';
+import 'package:todomodu_app/features/todo/presentation/providers/edit_todo_viewmodel_provider.dart';
 import 'package:todomodu_app/features/todo/presentation/widgets/subtask/subtask_item.dart';
 import 'package:todomodu_app/features/user/domain/entities/user_entity.dart';
 import 'package:todomodu_app/shared/constants/app_colors.dart';
@@ -9,63 +11,53 @@ import '../../providers/subtask/subtask_stream_provider.dart';
 import 'package:todomodu_app/features/todo/presentation/viewmodels/subtask_viewmodel.dart';
 
 class EditSubtaskList extends ConsumerWidget {
-  final String projectId;
-  final String todoId;
+  final Todo todo;
   final List<UserEntity> projectMembers;
 
   const EditSubtaskList({
     super.key,
-    required this.projectId,
-    required this.todoId,
+    required this.todo,
     required this.projectMembers,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final asyncSubtasks = ref.watch(subtaskStreamProvider((projectId, todoId)));
-    final viewModel = ref.watch(subtaskViewModelProvider);
+    final viewModel = ref.watch(editTodoViewModelProvider(todo).notifier);
+    final state = ref.watch(editTodoViewModelProvider(todo));
+    final subtasks = state.subtasks;
 
-    return asyncSubtasks.when(
-      data: (subtasks) {
-        return Column(
-          children: [
-            ...subtasks.map(
-              (subtask) => SubtaskItem(
-                key: ValueKey(subtask.id),
-                subtask: subtask,
-                projectMembers: projectMembers,
-                onChanged: (updated) => viewModel.update(updated),
-                onDelete: () => viewModel.delete(
-                  projectId: projectId,
-                  subtaskId: subtask.id,
-                ),
-              ),
+    return Column(
+      children: [
+        ...subtasks.map(
+          (subtask) => SubtaskItem(
+            key: ValueKey(subtask.id),
+            subtask: subtask,
+            projectMembers: projectMembers,
+            onChanged: viewModel.updateSubtask,
+            onDelete: () => viewModel.removeSubtask(subtask.id),
+          ),
+        ),
+        const SizedBox(height: 16),
+        Center(
+          child: IconButton(
+            icon: const Icon(
+              Icons.add_circle,
+              size: 48,
+              color: AppColors.primary200,
             ),
-            const SizedBox(height: 16),
-            Center(
-              child: IconButton(
-                icon: const Icon(
-                  Icons.add_circle,
-                  size: 48,
-                  color: AppColors.primary200,
-                ),
-                onPressed: () {
-                  final subtask = Subtask(
-                    id: const Uuid().v4(),
-                    title: '',
-                    isDone: false,
-                    todoId: todoId,
-                    projectId: projectId,
-                  );
-                  viewModel.create(subtask);
-                },
-              ),
-            ),
-          ],
-        );
-      },
-      loading: () => const CircularProgressIndicator(),
-      error: (e, _) => Text('오류: $e'),
+            onPressed: () {
+              final subtask = Subtask(
+                id: const Uuid().v4(),
+                title: '',
+                isDone: false,
+                todoId: todo.id,
+                projectId: todo.projectId,
+              );
+              viewModel.addSubtask(subtask);
+            },
+          ),
+        ),
+      ],
     );
   }
 }
